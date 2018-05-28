ad03110d3fcbb728db26f7e5333396da3f746ef7
WorawitO <33147910+worawito@users.noreply.github.com>
2018-05-28 07:25:27 +0000

    Create getCiscoSN.sh

---
+++ getCiscoSN.sh
@@ -0,0 +1,18 @@
+# Scripts for get SN by snmpwalk 
+# OID string for SN on cisco 
+# SN: 1.3.6.1.4.1.9.5.1.2.19
+# Model: 1.3.6.1.2.1.47.1.1.1.1.2.1001
+# Need list of device IP Address 
+########################################
+# Excample IPList contenet
+#/tmp/IPList
+#--BOF--
+#192.168.1.1
+#--EOF
+########################################
+
+# listSN.sh
+
+########################################
+!/bin/bash
+myfile/tmp/
